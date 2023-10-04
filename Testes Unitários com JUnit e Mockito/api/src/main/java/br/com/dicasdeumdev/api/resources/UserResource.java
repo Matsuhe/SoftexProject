fe6f2e3d6ffabe23c9d6f70234adc28a2abad0f5
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-04 19:31:22 -0300

    Retornando dto para user resource

--- Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/resources/UserResource.java
@@ -1,7 +1,8 @@
 package br.com.dicasdeumdev.api.resources;
 
-import br.com.dicasdeumdev.api.domain.Usuario;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.services.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,10 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
     @Autowired
+    private ModelMapper mapper;
+    @Autowired
     private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Usuario> findById(@PathVariable Integer id){
-         return ResponseEntity.ok().body(service.findById(id));
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
+         return ResponseEntity.ok().body(mapper.map(service.findById(id),UserDTO.class));
     }
 }
