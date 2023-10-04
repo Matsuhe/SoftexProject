f91e1a8de666ab01d638c5b7642a1bec1c96e73a
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-03 21:47:46 -0300

    "Refazendo: classe de dominio"

--- Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.api;
 
+import br.com.dicasdeumdev.api.domain.Usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class ApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+        Usuario usuario = new Usuario(1, "Matsuhe", "matsuhe@hotmail.com", "123");
     }
 
 }
---
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/domain/Usuario.java
@@ -0,0 +1,23 @@
+package br.com.dicasdeumdev.api.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Usuario {
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
