3d737dc4bf7aad13cd0e10c0a31f6a86aa68ea04
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-04 20:03:57 -0300

    ENDPOINT findall para user

--- Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
@@ -2,6 +2,9 @@ package br.com.dicasdeumdev.api.services;
 
 import br.com.dicasdeumdev.api.domain.Usuario;
 
+import java.util.List;
+
 public interface UserService {
     Usuario findById(Integer id);
+    List<Usuario> findAll();
 }
