30e037fc86e4bd289bbdf5fafba091f437002976
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-04 20:43:11 -0300

    ENDPOINT create para user

--- Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/UserService.java
@@ -1,10 +1,12 @@
 package br.com.dicasdeumdev.api.services;
 
 import br.com.dicasdeumdev.api.domain.Usuario;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 
 import java.util.List;
 
 public interface UserService {
     Usuario findById(Integer id);
     List<Usuario> findAll();
+    Usuario create(UserDTO obj);
 }
--- Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
+++ Testes Unitários com JUnit e Mockito/api/src/main/java/br/com/dicasdeumdev/api/services/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package br.com.dicasdeumdev.api.services.impl;
 
 import br.com.dicasdeumdev.api.domain.Usuario;
+import br.com.dicasdeumdev.api.domain.dto.UserDTO;
 import br.com.dicasdeumdev.api.repositories.UserRepository;
 import br.com.dicasdeumdev.api.services.UserService;
 import br.com.dicasdeumdev.api.services.exceptions.ObjectNotFoundException;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import java.util.Optional;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository repository;
+    @Autowired
+    private ModelMapper mapper;
     @Override
     public Usuario findById(Integer id) {
         Optional<Usuario> obj = repository.findById(id);
@@ -22,5 +26,10 @@ public class UserServiceImpl implements UserService {
 
     public List<Usuario> findAll(){
         return repository.findAll();
+    }
+
+    @Override
+    public Usuario create(UserDTO obj) {
+        return repository.save(mapper.map(obj, Usuario.class));
     }
 }
