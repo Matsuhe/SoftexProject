7d47cf2c384c33f078fde0be19fbb66ba20464cf
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-05 20:45:34 -0300

    teste para classe user service metodo update com sucesso

--- Testes Unitários com JUnit e Mockito/api/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
+++ Testes Unitários com JUnit e Mockito/api/src/test/java/br/com/dicasdeumdev/api/services/impl/UserServiceImplTest.java
@@ -115,6 +115,16 @@ class UserServiceImplTest {
 
     @Test
     void update() {
+        when(repository.save(any())).thenReturn(user);
+
+        Usuario response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(Usuario.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(name, response.getName());
+        assertEquals(email, response.getEmail());
+        assertEquals(password, response.getPassword());
     }
 
     @Test
