3ea3b70cd0d071e7078014d2b60e88af837674c0
Matheus Santos Volotão <matheus.volotao@hotmail.com>
2023-10-05 21:56:00 -0300

    teste para endpoint create da classe user resource implementado

--- Testes Unitários com JUnit e Mockito/api/src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
+++ Testes Unitários com JUnit e Mockito/api/src/test/java/br/com/dicasdeumdev/api/resources/UserResourceTest.java
@@ -93,6 +93,13 @@ class UserResourceTest {
 
     @Test
     void create() {
+        when(service.create(any())).thenReturn(user);
+
+        ResponseEntity<UserDTO> response = resource.create(userDTO);
+
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertNotNull(response.getHeaders().get("Location"));
     }
 
     @Test
